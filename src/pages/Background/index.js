89e1342012697963954f6c7a61825d2bcb371ebf
_MoonLight_ <moonlight8978@gmail.com>
2021-01-19 22:38:58 +0700

    Apply backup & Upload backup

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,5 +1,9 @@
 import '../../assets/img/icon-34.png';
 import '../../assets/img/icon-128.png';
 
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+chrome.runtime.onMessage.addListener(function ({ type, data }, sender, sendResponse) {
+  console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension');
+  if (type === 'upload') {
+    console.log(JSON.parse(data));
+  }
+});
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,13 +1,58 @@
 import React from 'react';
-import Button from '../../components/Button/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Popup.css';
+import { Button } from 'react-bootstrap';
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://api.github.com',
+});
+
+const uploadGist = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { type: 'getBackup' });
+  });
+};
+
+const downloadGist = () => {
+  client
+    .get(`/gists/${localStorage.getItem('_config/gistId')}`, {
+      headers: {
+        Authorization: `token ${localStorage.getItem('_config/githubToken')}`,
+      },
+    })
+    .then((response) => {
+      const json = response.data.files['yuanshen.site'].content;
+
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { type: 'apply', data: json });
+      });
+    });
+};
+
+const goToOptionsPage = () => {
+  chrome.tabs.create({ url: '/options.html' });
+};
 
 const Popup = () => {
   return (
-   <div>
-     <Button onClick={() => {}}>Upload progress</Button>
-     <Button onClick={() => {}}>Download progress</Button>
-   </div>
+    <>
+      <div className="popup__button-container">
+        <Button onClick={uploadGist} variant="success" className="popup__button">
+          Upload progress
+        </Button>
+
+        <Button onClick={downloadGist} variant="warning" className="popup__button">
+          Download progress
+        </Button>
+      </div>
+
+      <div className="popup__configure">
+        <Button onClick={goToOptionsPage} variant="link">
+          Configure
+        </Button>
+      </div>
+    </>
   );
 };
 
