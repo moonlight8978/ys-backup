71c1254befe474b2cfb054c60fe6f784ebaa0e3d
_MoonLight_ <moonlight8978@gmail.com>
2021-01-20 11:09:19 +0700

    Make the extension run

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,9 +1,39 @@
+import axios from 'axios';
+
 import '../../assets/img/icon-34.png';
 import '../../assets/img/icon-128.png';
 
-chrome.runtime.onMessage.addListener(function ({ type, data }, sender, sendResponse) {
+const client = axios.create({
+  baseURL: 'https://api.github.com',
+});
+
+chrome.runtime.onMessage.addListener(({ type, data }, sender, sendResponse) => {
   console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension');
   if (type === 'upload') {
-    console.log(JSON.parse(data));
+    client
+      .patch(
+        `/gists/${localStorage.getItem('_config/gistId')}`,
+        {
+          files: {
+            'yuanshen.site': {
+              content: data,
+            },
+          },
+          description: 'Auto update',
+        },
+        {
+          headers: {
+            Authorization: `token ${localStorage.getItem('_config/githubToken')}`,
+            Accept: 'application/vnd.github.v3+json',
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
+  return true;
 });
